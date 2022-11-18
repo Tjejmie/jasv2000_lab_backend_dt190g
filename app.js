92b9c6d1ede50b6107534f0b70ec820fbf850552
Tjejmie <jamie_svanberg@hotmail.com>
2022-11-18 16:09:27 +0100

    /api/courses but without subjects added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 
+
 // Create an Express application
 const app = express();
 app.use(cors());  // CORS-enabled for all origins!
@@ -9,13 +10,31 @@ app.use(cors());  // CORS-enabled for all origins!
 // Define the port the server will accept connections on
 const port = process.env.PORT || 3000;
 
+
 // Start the server
 app.listen(port, function() {
     console.log(`Server is running on port ${port}`);
 });
 
-// Define a route handler for GET requests to the web root
-// TODO: In lab 1, remove before submission
-app.get('/', function(req, res) {
-    res.send({ "message": "Hello, World!" });
+var jsonfile = require("jsonfile");
+var file = "miun-db.json";
+var courses = [];
+
+jsonfile.readFile(file, function(err, obj){
+    if(err){
+        console.log(err)
+    }
+    else{
+
+        courses = obj;
+    }
 });
+
+// Define a route handler for GET requests to the web root
+// Get all MIUN-courses
+app.get("/api/courses", function(req, res){
+
+    res.send(JSON.stringify(courses.courses, null, 2));
+})
+
+
